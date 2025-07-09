2c3c4bd6acf1a86a0b4b868c3945f190b883f414
mahathirrizky <mahathirrizky@gmail.com>
2025-07-10 05:35:25 +0800

    fix: Resolve FOUC by preloading CSS
    - Move CSS import from main.js to index.html to prevent Flash of Unstyled Content.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
-import './style.css';
+
 import App from './App.vue';
 import router from './router'; // Main router
 import routeradmin from './router/admin'; // Admin router
